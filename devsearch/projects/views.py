96b189ec42e72bed6a0961f5f01d3a00e22a1194
keshav <keshavsharma1196@gmail.com>
2022-12-13 21:04:57 +0530

    Views And Urls/ setting up inital views and urls

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def projects(request):
+    return HttpResponse("Projects")
+
+
+def project(request, pk):
+    return HttpResponse("Project" + pk)
