5df57ca11aaa9d93ab6041c07a5aef37b46142d0
keshav <keshavsharma1196@gmail.com>
2022-12-14 22:53:26 +0530

    ModelForm/creating forms using the concept of modelform, sending form to the templates for the output

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
+from .form import ProjectForm
 
 # Create your views here.
 
@@ -15,3 +16,9 @@ def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     context = {"project": projectObj}
     return render(request, "projects/single-project.html", context)
+
+
+def createProject(request):
+    form = ProjectForm()
+    context = {"form": form}
+    return render(request, "projects/project_form.html", context)
