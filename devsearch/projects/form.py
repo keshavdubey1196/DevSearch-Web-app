28998d4cade840036ab79cac685607c8969db1da
keshav <keshavsharma1196@gmail.com>
2022-12-15 19:54:00 +0530

    ThemeInstallation/Installing the theme, changing the templates,updating form.py

--- devsearch/projects/form.py
+++ devsearch/projects/form.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Project
+from django import forms
 
 
 class ProjectForm(ModelForm):
@@ -13,3 +14,19 @@ class ProjectForm(ModelForm):
             "source_link",
             "tags",
         ]
+        widgets = {
+            "tags": forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        # self.fields["title"].widget.attrs.update(
+        #     {"class": "input", "placeholder": "Add title"}
+        # )
+        for name, field in self.fields.items():
+            field.widget.attrs.update(
+                {
+                    "class": "input",
+                }
+            )
