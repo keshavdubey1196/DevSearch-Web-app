688d661970dd050b0da9f1fc798f5ffc73cae28b
keshav <keshavsharma1196@gmail.com>
2022-12-16 18:45:13 +0530

    Signals/adding signals to user app, signals will show whenever a profile,user is created,updated or deleted,refer to django signals website

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+
 # Create your models here.
 
 
@@ -30,7 +31,7 @@ class Profile(models.Model):
     )
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
