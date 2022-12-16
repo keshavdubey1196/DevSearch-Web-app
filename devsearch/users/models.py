81e0d3149f80facf5edab191e1950f79dc0f4428
keshav <keshavsharma1196@gmail.com>
2022-12-16 12:14:19 +0530

    Add and Render Profile/Adding skill model, connecting it with profile model, rendering the skills in template (skill_set.all)

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -9,6 +9,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     username = models.CharField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=200, null=True, blank=True)
     short_intro = models.CharField(max_length=500, null=True, blank=True)
     bio = models.TextField(max_length=1000)
@@ -30,3 +31,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
+
+    def __str__(self):
+        return str(self.name)
