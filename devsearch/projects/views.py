fefc6da4af8697d26b22b92e0a785c9882ee63a2
keshav <keshavsharma1196@gmail.com>
2022-12-16 21:29:50 +0530

    Authentication/user login, logout and flash messages

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
 from .form import ProjectForm
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
@@ -18,6 +20,7 @@ def project(request, pk):
     return render(request, "projects/single-project.html", context)
 
 
+@login_required(login_url="login")
 def createProject(request):
     form = ProjectForm()
     if request.method == "POST":
@@ -29,6 +32,7 @@ def createProject(request):
     return render(request, "projects/project_form.html", context)
 
 
+@login_required(login_url="login")
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
@@ -41,6 +45,7 @@ def updateProject(request, pk):
     return render(request, "projects/project_form.html", context)
 
 
+@login_required(login_url="login")
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
     if request.method == "POST":
--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 from .models import Profile
+from django.contrib import messages
 
 # Create your views here.
+
+
+def loginUser(request):
+    if request.user.is_authenticated:
+        return redirect("profiles")
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        try:
+            user = User.objects.get(username=username, password=password)
+        except:
+            messages.error(request, "User does'not exist")
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect("profiles")
+        else:
+            messages.error(request, "Username or password incorrect!")
+    return render(request, "users/login_register.html")
+
+
+def logoutUser(request):
+    logout(request)
+    messages.error(request, "User successfully logged out")
+    return redirect("login")
 
 
 def profiles(request):
