fbb6f1368c4477f089e9d867add7b046e410ca95
keshav <keshavsharma1196@gmail.com>
2022-12-18 20:44:04 +0530

    ADD,DELETE &EDIT skill

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -30,7 +30,7 @@ def createProject(request):
             project = form.save(commit=False)
             project.owner = profile
             project.save()
-            return redirect("projects")
+            return redirect("account")
     context = {"form": form}
     return render(request, "projects/project_form.html", context)
 
@@ -44,7 +44,7 @@ def updateProject(request, pk):
         form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
             form.save()
-            return redirect("projects")
+            return redirect("account")
     context = {"form": form}
     return render(request, "projects/project_form.html", context)
 
@@ -57,4 +57,4 @@ def deleteProject(request, pk):
         project.delete()
         return redirect("projects")
     context = {"object": project}
-    return render(request, "projects/delete_template.html", context)
+    return render(request, "delete_template.html", context)
