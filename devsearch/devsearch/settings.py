ffd005d81ce3339a88fdd9680e12ff2280e1b683
keshav <keshavsharma1196@gmail.com>
2022-12-26 00:16:24 +0530

    connecting postgres database, s3 buckets, setting up aws services,conneting them to the prjoect

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "users.apps.UsersConfig",
     "rest_framework",
     "corsheaders",
+    "storages",
 ]
 
 REST_FRAMEWORK = {
@@ -124,10 +125,33 @@ WSGI_APPLICATION = "devsearch.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "devsearch",
+#         "USER":"postgres",
+#         "PASSWORD":"KS146@123",
+#         "HOST": "localhost",
+#         "PORT":"5432",
+#     }
+# }
+
+# AWS
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "devsearch",
+        "USER":"keshavdubey",
+        "PASSWORD":"KS146123",
+        "HOST": "database-1.clece6pnoeyu.ap-northeast-1.rds.amazonaws.com",
+        "PORT":"5432",
     }
 }
 
@@ -189,3 +213,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_QUERYSTRING_AUTH=False
+
+AWS_S3_FILE_OVERWRITE=False
+
+AWS_ACCESS_KEY_ID='AKIA2SLDKQM7W6DN24XO'
+
+AWS_SECRET_ACCESS_KEY='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'
+
+AWS_STORAGE_BUCKET_NAME='devsearch-buckettt'
